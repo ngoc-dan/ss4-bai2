8b8d5c9f4eabc815be14c84e7062a7b101e656af
ngoc-dan <ngocdan191026@gmail.com>
2024-11-24 22:42:40 +0700

    Add files via upload

--- 4.2.cpp
+++ 4.2.cpp
@@ -3,12 +3,12 @@
 int main(){
 	int n;
 	printf("nhap so n = ");
-	scanf("%d" , n);
+	scanf("%d" , &n);
 	if(n%2==0){
 		printf("n la so chan\n");
 	}
 	else{
-	    printf("n la so le\n", n);
+	    printf("n la so le\n");
     }
 	return 0;
 }
